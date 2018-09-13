dbc1c8ddb2e0dd07be65e7d3540ff0be32ee8857
Kenneth <kenneth.cookie@gmail.com>
2018-09-12 21:02:27 -0400

    Update BananaBot.js

--- BananaBot.js
+++ BananaBot.js
@@ -1,18 +1,19 @@
-/*
-#
-#
-# Currently being written in Node.JS
-#
-*/
-/*
- Import:
+//-------------------------------------------------------------------
+//
+// BananaBot(tm) was created by CookieKenneth and is used for private
+// purposes dedicated to BananaHouse, LLC.
+// 
+// TOKEN: {token goes here}
+//
+//-------------------------------------------------------------------
+const { Client } = require('discord.js')
+const bot = new Client();
+const cfg = require('./config.json');
 
+bot.on('ready', () => {
+    console.log('Line One');
+    console.log('Line Two');
+    console.log('Line Three!');
+});
 
-
- Startup:
-
-
-
-
- Token: 
-/*
+bot.login(cfg.token); 
